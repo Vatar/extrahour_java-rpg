2a3100833de47b2501ebb6b41b8e02e401c7b8b6
Dan <daniel.atack@hotmail.fr>
2017-01-31 21:28:03 +0100

    package architecture :lollipop:

---
+++ src/monstres/Monstre.java
@@ -0,0 +1,5 @@
+package monstres;
+
+public interface Monstre {
+
+}
---
+++ src/systeme/Launcher.java
@@ -0,0 +1,14 @@
+package systeme;
+
+public class Launcher {
+
+	public Launcher() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
